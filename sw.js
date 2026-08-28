713c21de9eaec386c507820646aa044c2cbc681e
88ds8dnnyn-sudo <88ds8dnnyn@privaterelay.appleid.com>
2026-08-28 10:42:59 +0800

    部署: sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+/* 教师工作台 Service Worker：离线缓存 + 静默更新 */
+const CACHE = 'tw-v3';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      if (hit) {
+        // 后台刷新缓存，下次生效
+        fetch(e.request).then((res) => { if (res && res.ok) caches.open(CACHE).then((c) => c.put(e.request, res.clone())); }).catch(() => {});
+        return hit;
+      }
+      return fetch(e.request).then((res) => {
+        const cp = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, cp));
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
